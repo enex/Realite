eaa592aaf47c4cf8f26b2a239f51bcbbbdade353
Simon Vetter <simon.vetter@clye.app>
2025-08-15 12:43:00 +0200

    migrate to bun and implement base stuff

---
+++ server.ts
@@ -0,0 +1,35 @@
+import { createRequestHandler } from "@expo/server/adapter/bun";
+
+const CLIENT_BUILD_DIR = `${process.cwd()}/dist/client`;
+const SERVER_BUILD_DIR = `${process.cwd()}/dist/server`;
+const handleRequest = createRequestHandler({ build: SERVER_BUILD_DIR });
+
+const port = process.env.PORT || 3000;
+
+Bun.serve({
+  port: process.env.PORT || 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+    console.log("Request URL:", url.pathname);
+
+    const staticPath = url.pathname === "/" ? "/index.html" : url.pathname;
+    const file = Bun.file(CLIENT_BUILD_DIR + staticPath);
+
+    if (await file.exists()) return new Response(await file.arrayBuffer());
+
+    return handleRequest(req);
+  },
+  websocket: {
+    open(ws) {
+      console.log("WebSocket opened");
+    },
+    message(ws, message) {
+      console.log("WebSocket message:", message);
+    },
+    close(ws) {
+      console.log("WebSocket closed");
+    },
+  },
+});
+
+console.log(`Bun server running at http://localhost:${port}`);
